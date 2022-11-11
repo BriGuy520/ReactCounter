97718fa1b36e6f5453503deb62f3d71abd4cee19
Brian Falasz <bfalasz@yahoo.com>
2022-11-10 22:23:34 -0600

    added click to all buttons. now we can create any value we want as a string

--- src/Components/Numbers.js
+++ src/Components/Numbers.js
@@ -21,17 +21,17 @@ function Numbers({selectNumber = f => f}){
       <div>
       <button onClick={handleClick}>7</button>
       <button onClick={handleClick}>8</button>
-      <button>9</button>
+      <button onClick={handleClick}>9</button>
      </div>
      <div>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <button onClick={handleClick}>4</button>
+      <button onClick={handleClick}>5</button>
+      <button onClick={handleClick}>6</button>
      </div>
      <div>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <button onClick={handleClick}>1</button>
+      <button onClick={handleClick}>2</button>
+      <button onClick={handleClick}>3</button>
      </div>
      
     </div>
